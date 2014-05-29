69e7d46f5579f23c5f7c63a4b7215ef1f7977f2d
anderslime <aemilnielsen@gmail.com>
2014-05-29 16:30:23 +0200

    Courses with unique course number

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ActiveRecord::Base
+  validates_uniqueness_of :course_number
+
   translates :title, :teaching_form, :duration, :participant_limit, :registration,
              :course_objectives, :exam_schedule, :learn_objectives, :content,
              :litteratur, :remarks, :top_comment, :former_course, :exam_form,
