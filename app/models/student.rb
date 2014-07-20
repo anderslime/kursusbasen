3a51a4c747ff09ad7757e1f31101edeb0629a7d9
anderslime <aemilnielsen@gmail.com>
2014-07-20 13:40:05 +0200

    Simplify student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,11 @@ class Student < ActiveRecord::Base
   to_param :student_number
 
   has_many :course_plannings
+  has_many :planned_courses, through: :course_plannings, source: :course
 
   validates_uniqueness_of :student_number
 
   def plans_to_attend_course?(course)
-    course_plannings.map(&:course_id).include?(course.id)
+    planned_courses.include?(course)
   end
 end
