10015c383ae3f1e98456455a6033a3fb776007d6
anderslime <aemilnielsen@gmail.com>
2014-08-01 21:38:03 +0200

    Fix it all

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 
-  unless Rails.env.production?
-    namespace :api do
-      resources :students
-      resources :course_autocompletes, only: [:index]
-      resources :course_plannings, except: [:index, :new, :edit]
-      resources :schedule_groups, only: [:index]
-      resources :courses, only: [:index, :show]
-      resources :special_courses, only: [:create, :show]
-    end
-    resources :students, only: [:show]
+  namespace :api do
+    resources :students
+    resources :course_autocompletes, only: [:index]
+    resources :course_plannings, except: [:index, :new, :edit]
+    resources :schedule_groups, only: [:index]
+    resources :courses, only: [:index, :show]
+    resources :special_courses, only: [:create, :show]
   end
+  resources :students, only: [:show]
 
   root :to => "courses#index"
 
